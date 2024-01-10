eabb37b6389a5469b26073f31afeb038da8513d7
Marta <marta.af.lab@gmail.com>
2024-01-10 22:59:35 +0100

    issues

--- main.js
+++ main.js
@@ -1,65 +1,74 @@
-'use strict'
+'use strict';
 
 // QUERYSELECTOR
 const botton = document.querySelector('.js_botton');
-// evento click
-// contador
+const botReset = document.querySelector('.js_botReset');
+const endGameMessage = document.querySelector('.js_endGame'); 
 const inputNumber = document.querySelector('.js_input_number');
-
-// recoger su value y guardar en constante
 const test = document.querySelector('.js_test');
-// necesito el value y condicionales
-
 const numTry = document.querySelector('.js_num_try');
 const result = document.querySelector('.js_result');
 
-
-
 // VARIABLES DE DATOS
-let contador = 0; //contador
-const numberRandon = getRandomNumber (100);
-console.log(numberRandon);
+let contador = 0; // contador
+let numberRandom = getRandomNumber(100);
+console.log(numberRandom);
 
 // FUNCIONES
-function getRandomNumber(max) { 
-    return Math.ceil(Math.random() * max); 
-  }
+function getRandomNumber(max) {
+    return Math.ceil(Math.random() * max);
+}
 
-// funcion de comparar randon y el escrito
-function compareNumber () {
-    
-    const userNumber = inputNumber.value;
-    if( numberRandon === userNumber) {
-        result.innerHTML = 'Has ganado campeona!!';
-     }
-   else if(numberRandon > userNumber) {
-        test.innerHTML = 'Demasiado bajo';
-    }
-    else if(numberRandon < userNumber) {
-        test.innerHTML = 'Demasiado alto';
-    }
-    else if(numberRandon > 100 ) {
-        test.innerHTML = 'el numero debe estar entre 1 y 100';
-    }
-    else {
-        test.innerHTML = 'Escribe un numero';
+function showMessage(message) {
+    test.innerHTML = message;
+}
+
+// Función Reset
+function resetGame() {
+    contador = 0;
+    numTry.innerHTML = 'Número de intentos: ' + contador;
+    numberRandom = getRandomNumber(100);
+    console.log(numberRandom);
+    showMessage('¡Juego reiniciado!');
+}
+
+// Función comoprar
+function compareNumber() {
+    const userNumber = parseInt(inputNumber.value);
+
+    if (isNaN(userNumber)) {
+        showMessage('Escribe un número válido.');
+    } else {
+        if (numberRandom === userNumber) {
+            showMessage('¡Has ganado campeona!');
+            resetGame();
+        } else if (numberRandom > userNumber) {
+            showMessage('Demasiado bajo');
+        } else if (numberRandom < userNumber) {
+            showMessage('Demasiado alto');
+        } else if (numberRandom > 100) {
+            showMessage('El número debe estar entre 1 y 100');
+        }
     }
 }
 
-// EVENTO
-const handleClickBotton = (event) =>{
+// Función fin del juego
+function endGame() {
+    if (contador >= 10) {
+        endGameMessage.innerHTML = 'El juego ha terminado';
+        resetGame();
+    }
+}
+
+// EVENTOS
+botReset.addEventListener('click', resetGame);
+
+const handleClickButton = (event) => {
     event.preventDefault();
-    
-    compareNumber();// comparar y escribir mensaje
-    numberRandon //crear numero aleatorio
-    //contador
+    compareNumber();
+    endGame();
     contador++;
-    numTry.innerHTML = 'Numero de intentos :' + contador;
-    
-    //contador
+    numTry.innerHTML = 'Número de intentos: ' + contador;
+};
 
-}
-
-botton.addEventListener('click', handleClickBotton);
-
-
+botton.addEventListener('click', handleClickButton);
